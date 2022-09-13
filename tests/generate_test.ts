a6998e0b84520d4c7e9fd9734638f4ab0c004935
Ifiok Jr <ifiokotung@gmail.com>
2022-09-13 13:46:20 +0100

    test: sort snapshots alphabetically

--- tests/generate_test.ts
+++ tests/generate_test.ts
@@ -16,16 +16,25 @@ for await (
     );
     const workflows: DefineWorkflowOutput = result.default;
     await generate(workflows);
+    const content = new Map<string, string>();
 
     for await (const entry of Deno.readDir(workflows.rootDirectory)) {
       if (!entry.isFile || !entry.name.endsWith(".yml")) {
         continue;
       }
 
-      await snapshot(
-        t,
+      content.set(
+        entry.name,
         await Deno.readTextFile(path.join(workflows.rootDirectory, entry.name)),
       );
+    }
+
+    const entries = Object.entries(content).sort((a, z) => {
+      return a[0].localeCompare(z[0]);
+    });
+
+    for (const [, text] of entries) {
+      await snapshot(t, text);
     }
   });
 }
